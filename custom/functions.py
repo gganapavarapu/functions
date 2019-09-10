907008b647316519d6439bb4f9955a1ed2a6c4e9
Venkata Sitaramagiridharganesh Ganapavarapu <giridhar.ganapavarapu@ibm.com>
2019-09-09 20:36:34 -0400

    updated functions

--- custom/functions.py
+++ custom/functions.py
@@ -55,25 +55,26 @@ class HelloWorldGG(BaseTransformer):
         try:
             print("srom pre version")
             logger.error("no error srom")
-            if(not self.token):
-                logger.error("No SROM token")
-            # package = "https://srompypyproxy.mybluemix.net/{}".format(self.token)
-            # pip(["install",
-            #         "--extra-index-url",
-            #         package,
-            #         "srom[utils,core,preprocessing]==1.2.0rc12"])
-            # import srom
-            # srom_version = srom.__version__
-            # print("srom version: ", srom_version)
-            try:
-                import couchdb
-            except:
-                logger.error("couchdb not found")
-                pip(["install",
-                     "CouchDB"])
-                import couchdb
+            couch_version = 0.0
+            # if(not self.token):
+            #     logger.error("No SROM token")
+            # # package = "https://srompypyproxy.mybluemix.net/{}".format(self.token)
+            # # pip(["install",
+            # #         "--extra-index-url",
+            # #         package,
+            # #         "srom[utils,core,preprocessing]==1.2.0rc12"])
+            # # import srom
+            # # srom_version = srom.__version__
+            # # print("srom version: ", srom_version)
+            # try:
+            #     import couchdb
+            # except:
+            #     logger.error("couchdb not found")
+            #     pip(["install",
+            #          "CouchDB"])
+            #     import couchdb
             
-            couch_version = couchdb.__version__
+            # couch_version = couchdb.__version__
             msg = "test"
             if(self.name is None):
                 msg = "Hello! How are you? your couchdb version is {}".format(couch_version)
